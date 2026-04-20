77c33e1c777543617dac2c37a21d512f0f10bb24
Kevin Berfirer <kberfirer@gmail.com>
2026-04-20 09:17:44 -0400

    Initial Commit

---
+++ Task_30_Git/Task_30_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_30_GitApp.swift
+//  Task_30_Git
+//
+//  Created by Kevin Berfirer on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Task_30_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
